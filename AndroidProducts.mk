e694fc815234a54b349b44e3cf8ad924f5b5f455
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-06-18 03:01:56 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,8 +17,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_merlin.mk
+    $(LOCAL_DIR)/twrp_merlin.mk
 
 COMMON_LUNCH_CHOICES := \
-	omni_merlin-userdebug \
-	omni_merlin-eng
+	twrp_merlin-eng
