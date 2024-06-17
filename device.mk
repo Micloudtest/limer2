d3c6ca88908369e35e8759072b194913222c4317
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-06-18 02:59:30 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,5 +16,42 @@
 # limitations under the License.
 #
 
+
 LOCAL_PATH := device/xiaomi/merlin
 
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.bootimage.build.date.utc=0 \
+    ro.build.date.utc=0
+
+PRODUCT_PACKAGES += \
+    libshim_beanpod
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Keymaster
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
