b836ca1d8fdcaa18590c0c38ff7822ca8c5ce9d6
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-06-18 02:47:35 +0800

    Update and rename omni_merlin.mk to twrp_merlin.mk

--- twrp_merlin.mk
+++ twrp_merlin.mk
@@ -20,20 +20,17 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from merlin device
 $(call inherit-product, device/xiaomi/merlin/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := omni_merlin
+PRODUCT_NAME := twrp_merlin
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi Note 9
 PRODUCT_MANUFACTURER := xiaomi
